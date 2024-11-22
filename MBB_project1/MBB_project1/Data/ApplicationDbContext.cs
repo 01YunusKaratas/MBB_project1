526f8b02ab2987aaca47b6311d9c655550481fbf
YUNUS EMRE KARATAS <yunuskaratas589@gmail.com>
2024-11-22 22:53:57 +0300

    MBB_project1

---
+++ MBB_project1/MBB_project1/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MBB_project1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBB_project1.Data
+{
+    public class ApplicationDbContext:DbContext //entity framework ile ilgil
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+
+        public DbSet<Product> product_table{ get; set;}    
+        public DbSet<User> user_table { get; set; } 
+    }
+}
---
+++ MBB_project1/MBB_project1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MBB_project1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
