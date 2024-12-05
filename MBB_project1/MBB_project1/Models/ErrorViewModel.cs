0edad79298278732dfb791b0292d74ca96074951
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-12-05 10:43:18 +0300

    Delete MBB_project1 directory

--- MBB_project1/MBB_project1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MBB_project1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
