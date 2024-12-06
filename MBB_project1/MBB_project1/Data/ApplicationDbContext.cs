486551ad0466b34a27f37e20d09d8fed21d40672
01YunusKaratas <yunuskaratas589@gmail.com>
2024-12-06 10:21:59 +0300

    MBB_Project
    Manisa BüyükŞehir belediyesinde ilk çalışmam .Asp.net ve MVC mimarisine giriş için bir depo uygulaması yaptım.Bu depo uygulamasında admin alına ürünleri depoya kaydedebiliyor daha sonra alınan ürünlerinin sayısını güncelleyebilir ,silebilir .Bu uygulama sayesinde Asp.net' e  hızlı bir giriş yaptım.

---
+++ MBB_project1/MBB_project1/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using MBB_project1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBB_project1.Data
+{
+    public class ApplicationDbContext:DbContext //entity framework ile ilgil
+    {
+        public ApplicationDbContext() //Constructor
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+        //dataya bağlanmak için 
+
+        public DbSet<Product> product_table{ get; set;} //Bu kodu Product_Tablela bağlantı sağlamak için yazdık
+        public DbSet<User> user_table { get; set; }  //Bu koduda User_table sınıfıyla bağlantı sağlamak için yaptık
+    }
+}
+
+
---
+++ MBB_project1/MBB_project1/Program.cs
@@ -0,0 +1,47 @@
+using MBB_project1.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Bu kod bloğunu HttpEklentisi için koydum
+builder.Services.AddDistributedMemoryCache(); // Cache'i ekleyin
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session süresi
+    options.Cookie.HttpOnly = true; // Güvenlik için
+    options.Cookie.IsEssential = true; // Cookie'nin temel olması gerektiğini belirtir
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// DbContext'i ekle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Oturumları etkinleştir
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
